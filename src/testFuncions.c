15fc3fddb03b2e97ffa487c42874d9cf8eb06ff0
dragonxblue2005 <diazrubiohugo@gmail.com>
2025-05-16 00:02:20 +0200

    Implementacion tests

--- src/testFuncions.c
+++ src/testFuncions.c
@@ -23,7 +23,7 @@ int main(){
 
     insercioParam(w, x, z, y);
 
-     //test de evaluaFormula()
+     //test de evaluaFormula() y init_poblacion()
     int **poblacion = malloc(nCromosomes * sizeof(int*));
     int *fitness    = malloc(nCromosomes * sizeof *fitness);
     int **seleccionados = malloc(nCromosomes * sizeof(int*));
@@ -53,8 +53,42 @@ int main(){
         printf("%d,", fitness[i]);
     }
 
+    //Test seleccionar_padres()
+    seleccionar_padres(poblacion, fitness, seleccionados, nCromosomes, kParametre);
+    printf("Padres seleccionados (primer gen de cada uno):");
+    for (int i = 0; i < nCromosomes; i++) {
+        printf(" %d", seleccionados[i][0]);
+    }
+    printf("\n\n");
+
+        //Test imprimir_estado()
+    printf("Estado generación 0:\n");
+    imprimir_estado(0, fitness[0], 0, poblacion[0]);
+    printf("\n");
+
     //test one point crossover
     
+
+
+ //Test ejecutar_GA()
+    int *mejor = ejecutar_GA(poblacion, fitness, seleccionados, poblacion_nueva, nGeneracions, nCromosomes, probMutacio, kParametre);
+    printf("Mejor cromosoma final:\n");
+    for (int j = 0; j < NUM_GENS; j++) {
+        printf("%d", mejor[j]);
+    }
+    printf("\n\n");
+
+    //Liberar memoria mejor
+    free(mejor);
+
+        //Test mutar()
+    printf("Test mutar():\n");
+    for (int g = 0; g < 5; g++) {
+        int original = rand() % 2;
+        int m = mutar(original, probMutacio);
+        printf("  gen=%d -> mutado=%d\n", original, m);
+    }
+    printf("\n");
 
 
     return 0;
